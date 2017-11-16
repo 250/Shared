75df938e4a236dd4027f6098766c016a5cc35a26
Bilge <bilge@scriptfusion.com>
2017-11-16 04:25:45 +0000

    Added Laplace, Torn and Dirichlet Prior algorithms.

--- src/Algorithm.php
+++ src/Algorithm.php
@@ -8,9 +8,19 @@ use Eloquent\Enumeration\AbstractEnumeration;
 /**
  * @method static WILSON()
  * @method static BAYESIAN()
+ * @method static TORN()
+ * @method static LAPLACE()
+ * @method static LAPLACE_LOG()
+ * @method static DIRICHLET_PRIOR()
+ * @method static DIRICHLET_PRIOR_LOG()
  */
 final class Algorithm extends AbstractEnumeration
 {
     public const WILSON = 'WILSON';
     public const BAYESIAN = 'BAYESIAN';
+    public const TORN = 'TORN';
+    public const LAPLACE = 'LAPLACE';
+    public const LAPLACE_LOG = 'LAPLACE_LOG';
+    public const DIRICHLET_PRIOR = 'DIRICHLET_PRIOR';
+    public const DIRICHLET_PRIOR_LOG = 'DIRICHLET_PRIOR_LOG';
 }
